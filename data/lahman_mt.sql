f45d96af96e070673b8c8618616dea7a08956ecd
Michael Tatar <michael.tatar.jr@gmail.com>
2024-02-10 17:30:16 -0600

    Through Q2

--- data/lahman_mt.sql
+++ data/lahman_mt.sql
@@ -9,33 +9,42 @@
 
 
 -- 1. Find all players in the database who played at Vanderbilt University. Create a list showing each player's first and last names as well as the total 
--- salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
+-- -- salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
+-- David Price seems to have earned the most money.
 
--- SELECT DISTINCT(playerid)
--- FROM salaries
--- WHERE playerid = "zeidjo01"
-
-
-WITH vandy AS (SELECT
-*
-FROM collegeplaying
-WHERE schoolid ='vandy')
-SELECT
-p.namefirst,
-p.namelast,
-SUM(s.salary)
-FROM people as p
-JOIN vandy
-USING(playerid)
-JOIN salaries as s
-USING(playerid)
-GROUP BY 1,2
-ORDER BY AVG(s.salary) DESC;
+-- WITH vandy AS (SELECT
+-- *
+-- FROM collegeplaying
+-- WHERE schoolid ='vandy')
+-- SELECT
+-- p.namefirst,
+-- p.namelast,
+-- SUM(s.salary) AS total_salary
+-- FROM people as p
+-- JOIN vandy
+-- USING(playerid)
+-- JOIN salaries as s
+-- USING(playerid)
+-- GROUP BY 1,2
+-- ORDER BY SUM(s.salary) DESC;
 
 
 -- 2. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in -- 2016.
+
+SELECT 
+CASE WHEN pos='OF' THEN 'Outfield'
+	 WHEN pos IN ('SS', '1B', '2B', '3B') THEN 'Infield'
+	 WHEN pos IN ('P', 'C') THEN 'Battery'
+	 ELSE 'Benchwarmer' END AS group_position,
+	 COUNT(po) AS force_outs
+FROM fielding
+WHERE yearid = '2016'
+GROUP BY 1;
  
 -- 3. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends? (Hint: For this question, you might find it helpful to look at the generate_series function (https://www.postgresql.org/docs/9.1/functions-srf.html). If you want to see an example of this in action, check out this DataCamp video: https://campus.datacamp.com/courses/exploratory-data-analysis-in-sql/summarizing-and-aggregating-numeric-data?ex=6)
+
+
+
 
 -- 4. Find the player who had the most success stealing bases in 2016, where success is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted at least 20 stolen bases. Report the players' names, number of stolen bases, number of attempts, and stolen base percentage.
 
